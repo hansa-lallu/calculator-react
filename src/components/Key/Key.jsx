321f6a3d00a5269dc744a061fb378795f63cd155
hansa-lallu <hansalallu@gmail.com>
2020-05-11 09:51:38 +0100

    adding key css

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Key.css';
 
-const Key = ({ keyAction, keyType, keyValue }) => <div className="key-container" />;
+const Key = ({ keyAction, keyType, keyValue }) => (
+  <div className="key-container">
+    <p className="key-value">
+      {keyValue}
+    </p>
+  </div>
+);
 
 Key.propTypes = {
   keyAction: PropTypes.func.isRequired,
